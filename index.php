f26f3fa17254f15c81b960948332352aaa343638
SarungOverFlow <nurinawawy@protonmail.com>
2021-03-30 07:02:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Jekyll Blog Maker">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href='/assets/img/favicon.png'> 
+	<title> Jekyll Blog Maker </title>
+	<link href="/assets/css/materialize.min.css" rel="stylesheet" >
+</head>
+<body class="container">
+<?php
+
+	if ( isset ( $_POST['publish'] ) ) {
+
+		$title = $_POST['title'];
+		$content = $_POST['content'];
+		$category = $_POST['category'];
+		$filename = date('Y-m-d')."-".strtolower ( str_replace (" " , "-" , $title ) ) . ".markdown";
+		$date = date('Y-m-d h:i:s');
+
+	$fc = 
+"---
+layout: post
+title: {$title}
+date: {$date}
+categories: {$category}
+---
+{$content}
+";
+
+	file_put_contents ( $filename, $fc );
+		
+		
+	}
+     function ck_editor ($name=null,$val=null) {
+     		echo '<script src="/assets/js/ckeditor.js"></script>';
+     		echo '<div class="col s12 input-field">';
+     		echo "<textarea id='editor' name='content'>$val</textarea></div>";
+     		echo "<script> ";
+     		echo "ClassicEditor
+			        .create( document.querySelector( '#editor' ), {
+			        	 toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+
+			        })
+
+        			.catch( error => {
+			            console.error( error );
+			        } );";
+     		echo "</script>";
+     }
+
+	echo "<div class='center'>";
+	echo "<h4>Jekyll Blog Maker</h4>";
+	echo "</div>";
+	
+	echo "<form method='post' action=''>";
+	echo "<div class='row'>";
+
+	echo "<div class='col s12'>";
+	echo "<div class='input-field'>";
+	echo "<label>Judul Tulisan</label>";
+	echo "<input type='text' name='title' />";
+	echo "</div>";
+	echo "</div>";
+
+	echo "<div class='col s12'>";
+	echo "<div class='input-field'>";
+	echo "<label>Kategori Tulisan</label>";
+	echo "<input type='text' name='category' />";
+	echo "</div>";
+	echo "</div>";
+
+
+	ck_editor ( "content" );	
+
+
+	echo "<div class='col s12'>";
+	echo "<div class='input-field'>";
+	echo "<input type='submit' class='btn btn-small' name='publish' value='Terbitkan' />";
+	echo "</div>";
+	echo "</div>";
+
+	echo "</div>";
+	echo "</form>";
+?>
+
+		<script src="/assets/js/jquery.min.js"></script>
+		<script src="/assets/js/materialize.min.js" charset="utf-8"></script>
+		<script type="text/javascript">
+			document.addEventListener('DOMContentLoaded', function() {
+				M.AutoInit();
+		     });
+		</script>
+	</body>
+</html>
